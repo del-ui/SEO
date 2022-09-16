a5b48871c937552afe387366eb253146df548ee9
del-ui <wambuivictor@students.must.ac.ke>
2022-09-16 12:30:55 +0300

    commit

--- app/src/main/java/com/victor/seo/UserAdapter.kt
+++ app/src/main/java/com/victor/seo/UserAdapter.kt
@@ -1,12 +1,28 @@
 package com.victor.seo
 
+import android.content.Context
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class UserAdapter: RecyclerView.Adapter {
+class UserAdapter(val context: Context, val userList: ArrayList<user>):
+    RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
 
     class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-
+        val textName = itemView.findViewById<TextView>(R.id.txt_name)
 
     }
 
